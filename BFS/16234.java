baa42cc9513eac6961adb5400ae7f891e8ce091e
dasomh2 <34855192+dasomh2@users.noreply.github.com>
2018-12-27 19:55:16 +0900

    Update 16234.java

--- BFS/16234.java
+++ BFS/16234.java
@@ -29,6 +29,8 @@ public class Main {
 		}
 
 		while(true) {
+			//인구이동 할 수 있는만큼 이동 (현재 판에서)
+			//중복방지를 visit배열 하나로 함 -> 현재 점에서 방문하지 않은 점들은 어차피 재방문X 
 			visit = new boolean[N][N];
 
 			for(int i=0; i<N; i++) {
@@ -41,10 +43,12 @@ public class Main {
 				}
 			}
 
+			//인구이동이 전혀 없으면 바로 종료
 			if(!flag) {
 				System.out.println(cnt);
 				return;
 			}
+			
 			cnt++;
 			flag = false;
 		}
@@ -63,9 +67,9 @@ public class Main {
 		q.offer(new Point(x,y));
 		united.offer(new Point(x, y));
 
-		//중복 방문 안할라고 
-
 		visit[x][y] = true;
+		
+		//총 인구수 누적
 		res += map[x][y];
 
 		while(!q.isEmpty()) {
@@ -79,6 +83,8 @@ public class Main {
 
 				if(isRange(nx, ny) && !visit[nx][ny]) {
 					int dist = Math.abs(map[p.x][p.y]-map[nx][ny]);
+					
+					//범위 안에 들면 방문체크 & 누적 & 큐에 추가 
 					if(dist >= L && dist <= R) {
 						visit[nx][ny] = true;
 						res += map[nx][ny];
@@ -91,9 +97,11 @@ public class Main {
 
 		int size = united.size();
 
+		//united 사이즈가 1이면 국경선 열 나라가 없었다는 뜻 
 		if(size > 1) {
 			int value = res/size;
 
+			//값 갱신 
 			while(!united.isEmpty()) {
 				Point p = united.poll();
 				map[p.x][p.y] = value;
